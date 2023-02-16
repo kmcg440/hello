47db5bf2143c75c37eab36f419137689d748ddcb
Kacy McGrath <mcgrathk2@gmail.com>
2023-02-16 12:26:35 -0500

    yahoo add

--- hello.py
+++ hello.py
@@ -1,4 +1,46 @@
-print("hello github")
-print("another line")
-print('hello 3')
-print("hello4")
+from yahoo_oauth import OAuth2
+import yahoo_fantasy_api as yfa
+import time
+
+#connect to yahoo api
+
+sc =OAuth2(None, None, from_file='/home/kmcgrath/oauth2/yahoo/oauth2.json')
+
+#get game object
+gm = yfa.Game(sc, 'nba')
+
+leagues = gm.league_ids()
+
+print(leagues)
+
+#get the league object 
+lg = gm.to_league('418.l.69932')
+
+#get the team key
+teamkey = lg.team_key()
+
+#get the team object
+team = lg.to_team(teamkey)
+
+#get team roster
+roster = team.roster()
+
+print(roster)
+
+print("=== MY TEAM ===")
+
+for r in roster:
+    print(r)
+
+fa = lg.free_agents("G")
+
+print("=== FREE AGENTS ===")
+
+for p in fa:
+    print(p)
+
+# Drop Player
+#team.drop_player(6619)
+
+# Add Player
+#team.add_player(6169)
